f2de8c9fb53427881c6ffc28dd6beb637e06f0f1
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 11:24:36 +0000

    Change setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-touch $HOME/.bash_aliases
-echo "alias c='clear'" >> $HOME/.bash_aliases
-echo "alias rld='source ~/.bashrc'" >> $HOME/.bash_aliases
-echo "alias e='exit'" >> $HOME/.bash_aliases
-echo "alias dev='cd ~/workspace; pwd; ls -a'" >> $HOME/.bash_aliases
-echo "alias gs='git status'" >> $HOME/.bash_aliases
-echo "alias python='python3'" >> $HOME/.bash_aliases
-echo "alias ptn='python'" >> $HOME/.bash_aliases
+echo 'This is the log file echoed from dev-setup install script ('$0').' >> ~/dev-setup.log
