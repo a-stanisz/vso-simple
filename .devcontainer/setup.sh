d02b108b9665349c89b883357aaf2d650ac37b36
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 13:29:46 +0000

    Comment out .bash_aliases cp command

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,4 +3,4 @@
 LOG_DIR=''$HOME'/dev-setup.log'
 echo $(date) >> $LOG_DIR
 echo 'This is the log file echoed from dev-setup script ran by .devcontainer' >> $LOG_DIR
-cp ~/dotfiles/.bash_aliases ~/
+# cp ~/dotfiles/.bash_aliases ~/
