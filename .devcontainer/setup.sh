19a5f84a10b49d609e50256afdb8f6d96fc7f033
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 12:24:16 +0000

    Fix date output

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo '$date' >> ~/dev-setup.log
-echo 'This is the log file echoed from dev-setup install script ('$0').' >> ~/dev-setup.log
+LOG_DIR=''$HOME'/dev-setup.log'
+echo $(date) >> $LOG_DIR
+echo 'This is the log file echoed from dev-setup install script ('$0').' >> $LOG_DIR
