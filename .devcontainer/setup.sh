a55eb8c0f3803ce37a7bd392141afe5a97849b3e
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 12:32:36 +0000

    Minor changes in install script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,4 +2,4 @@
 
 LOG_DIR=''$HOME'/dev-setup.log'
 echo $(date) >> $LOG_DIR
-echo 'This is the log file echoed from dev-setup install script ('$0').' >> $LOG_DIR
+echo 'This is the log file echoed from dev-setup script ran by .devcontainer' >> $LOG_DIR
