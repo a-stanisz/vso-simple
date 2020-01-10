ee841ac9557b1e18fca82d4863267e63a297163c
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 13:58:19 +0000

    Remove uncommented lines

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,4 +3,3 @@
 LOG_DIR=''$HOME'/dev-setup.log'
 echo $(date) >> $LOG_DIR
 echo 'This is the log file echoed from dev-setup script ran by .devcontainer' >> $LOG_DIR
-# cp ~/dotfiles/.bash_aliases ~/
