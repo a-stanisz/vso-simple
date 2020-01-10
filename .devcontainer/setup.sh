28f5399ddccf8cc16f2944beb991c099d5bd3878
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 10:22:52 +0000

    Change aliases defs

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,8 +2,9 @@
 
 touch $HOME/.bash_aliases
 echo "alias c='clear'" >> $HOME/.bash_aliases
-echo "alias rld='source ~/.bashrc'; echo '.bashrc reloaded'" >> $HOME/.bash_aliases
+echo "alias rld='source ~/.bashrc'" >> $HOME/.bash_aliases
 echo "alias e='exit'" >> $HOME/.bash_aliases
-echo "alias dev='cd ~/workspace; ls -a'" >> $HOME/.bash_aliases
+echo "alias dev='cd ~/workspace; pwd; ls -a'" >> $HOME/.bash_aliases
 echo "alias gs='git status'" >> $HOME/.bash_aliases
 echo "alias python='python3'" >> $HOME/.bash_aliases
+echo "alias ptn='python'" >> $HOME/.bash_aliases
