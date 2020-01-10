dd312159c1780fc53475e63129093f6f71973d87
Adam Staniszewski <hello@adamstaniszewski.com>
2020-01-10 10:12:22 +0000

    Add more custom bash aliases

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 touch $HOME/.bash_aliases
+echo "alias c='clear'" >> $HOME/.bash_aliases
+echo "alias rld='source ~/.bashrc'; echo '.bashrc reloaded'" >> $HOME/.bash_aliases
+echo "alias e='exit'" >> $HOME/.bash_aliases
+echo "alias dev='cd ~/workspace; ls -a'" >> $HOME/.bash_aliases
+echo "alias gs='git status'" >> $HOME/.bash_aliases
 echo "alias python='python3'" >> $HOME/.bash_aliases
